44ac99d7bbb3f395ddd4c049a8cfe4cfa34e1919
Lucie-Lanssade <lucie.lanssade@gmail.com>
2023-03-15 16:10:55 +0100

    timestamp on profile

--- models/Profile.model.js
+++ models/Profile.model.js
@@ -1,17 +1,18 @@
 const { Schema, model } = require("mongoose");
 
-const profileSchema = new Schema({
-  user: { type: Schema.Types.ObjectId, ref: "User" },
-  gender: { type: String, required: true, enum: ["Man", "Woman"] },
-  age: { type: Number, required: true },
-  currentHeight: { type: Number, required: true },
-
-  currentWeight: { type: Number, required: true, decimal: 1 },
-
-  currentImc: { type: Number },
-  weightGoal: { type: Number, required: true },
-  dailyCaloriesGoal: { type: Number },
-});
+const profileSchema = new Schema(
+  {
+    user: { type: Schema.Types.ObjectId, ref: "User" },
+    gender: { type: String, required: true, enum: ["woman", "man"] },
+    age: { type: Number, required: true },
+    currentHeight: { type: Number, required: true },
+    currentWeight: { type: Number, required: true },
+    currentImc: { type: Number },
+    weightGoal: { type: Number, required: true },
+    dailyCaloriesGoal: { type: Number },
+  },
+  { timestamps: true }
+);
 
 const Profile = model("Profile", profileSchema);
 
